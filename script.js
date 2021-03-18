497dc61a294be8764c7c3e2eee449a0961943a1d
tati oliveira <tat1roliv@gmail.com>
2021-03-18 19:19:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ pizzaJson.map((item, index) => {
     let pizzaItem = selectHtml(".models .pizza-item").cloneNode(true);
 
     //puxando dados do Json
-    //imagem
-    pizzaItem.querySelector('pizza-item--img img').src= item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+        //imagem
+    pizzaItem.querySelector('pizza-item--img img').src= item.img;
 
     //preenche dados
     selectHtml(".pizza-area").append(pizzaItem);
