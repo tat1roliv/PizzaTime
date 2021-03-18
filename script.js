7a833db80e894b60400c0ffe1f87c0bb81826bb2
tati oliveira <tat1roliv@gmail.com>
2021-03-18 19:36:51 -0300

    fix json reference

--- script.js
+++ script.js
@@ -7,20 +7,18 @@ const selectHtmlAll = (elemento)=>{
     return document.querySelectorAll(elemento);
 }
 
-pizzaJson.map((item, index) => {
+pizzaJson.map((pizza, index) => {
     //clona a estrutura do html
     let pizzaItem = selectHtml(".models .pizza-item").cloneNode(true);
 
     //puxando dados do Json
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
-    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
-    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
-        //imagem
-    pizzaItem.querySelector('pizza-item--img img').src= item.img;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = pizza.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = pizza.description;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${pizza.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--img img').src = pizza.img;
 
     //preenche dados
     selectHtml(".pizza-area").append(pizzaItem);
-    
 
 
 });
